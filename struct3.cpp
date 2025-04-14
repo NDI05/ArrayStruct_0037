055a04fbbf261c73aea84bc84d945ba96b237b69
NDI05 <andhikap877@gmail.com>
2025-04-14 11:36:21 +0700

    feat: create looping for input data struct on c++

--- struct3.cpp
+++ struct3.cpp
@@ -2,16 +2,16 @@
 #include <string>
 using namespace std;
 
+struct Weapon {
+    string name;
+    int damage;
+};
+
 struct Player {
     string name;
     string id;
     string level;
     Weapon wp;
-};
-
-struct Weapon {
-    string name;
-    string damage;
 };
 
 int main(){
@@ -26,10 +26,17 @@ int main(){
     cin >> pl.id;
     cout << "Masukkan Level Player : ";
     cin >> pl.level;
+    cin.ignore();
+    cout << "Masukkan Nama Weapon : ";
+    getline(cin, pl.wp.name);
+    cout << "Masukkan Damage Weapon : ";
+    cin >> pl.wp.damage;
     // output data struct
     cout << "=========Output Data Struct========" << endl;
     cout << "Nama Player : " << pl.name << endl;
     cout << "ID Player : " << pl.id << endl;
     cout << "Level Player : " << pl.level << endl;
+    cout << "Nama Weapon Player : " << pl.wp.name << endl;
+    cout << "Damage Weapon Player : " << pl.wp.damage << endl;
     cout << "====================================" << endl;
 }
