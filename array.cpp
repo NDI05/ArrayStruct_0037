c23c5e6d170fefe528fc47052677870aa565141d
NDI05 <andhikap877@gmail.com>
2025-04-14 10:46:14 +0700

    feat: create global variabel

--- array.cpp
+++ array.cpp
@@ -2,6 +2,15 @@
 #include <string>
 using namespace std
 
+// declare array without size, must be initialized
+int nilai[] = {1,2,3};
+
+//declare array with size and initialize
+string nama[5] = {"ben", "agus", "fara", "ama", "sakila"};
+
+//declare array with size and no initialization
+float score[5];
+
 int main(){
     
 }
