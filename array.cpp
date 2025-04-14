aa6d1c700271371a9eba73eb4584c5b5e6bc37e2
NDI05 <andhikap877@gmail.com>
2025-04-14 11:06:19 +0700

    feat: create function to input (looping) variabel)

--- array.cpp
+++ array.cpp
@@ -18,6 +18,21 @@ int main(){
     nama[3] = "piska";
     cout << "=======This Is After Change=======" << endl;
     cout << "Nama pada index 4 adalah " << nama[3] << endl;
+    cout << endl;
 
+    cout << "==================================" << endl;
+    cout << "=====Input&Output Data Array======" << endl;
+    cout << "==================================" << endl;
+    // input data to array
+    for (int i = 0; i<5; i++){
+        cout << "Silahkan Masukkan Score Ke- " << i+1 << " : ";
+        cin >> score[i];
+    }
+    cout << "=========Output Data Array========" << endl;
+    // output data from array
+    for (int i = 0; i<5; i++){
+        cout << "Score Ke- " << i+1 << " : " << score[i] << endl;
+    }
+    cout << "==================================" << endl;
 
 }
