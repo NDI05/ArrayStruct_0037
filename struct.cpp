ba2e9d5a369a28a5292f8cd71690d15865aae2a1
NDI05 <andhikap877@gmail.com>
2025-04-14 11:15:29 +0700

    feat: create input and output on struct

--- struct.cpp
+++ struct.cpp
@@ -6,4 +6,24 @@ struct Player {
     string name;
     string id;
     string level;
+};
+
+int main(){
+    Player pl;
+    cout << "==================================" << endl;
+    cout << "=====Input&Output Data Struct======" << endl;
+    cout << "==================================" << endl;
+    // input data to struct
+    cout << "Masukkan Nama Player : ";
+    getline(cin, pl.name);
+    cout << "Masukkan ID Player : ";
+    getline(cin, pl.id);
+    cout << "Masukkan Level Player : ";
+    getline(cin, pl.level);
+    // output data struct
+    cout << "=========Output Data Struct========" << endl;
+    cout << "Nama Player : " << pl.name << endl;
+    cout << "ID Player : " << pl.id << endl;
+    cout << "Level Player : " << pl.level << endl;
+    cout << "====================================" << endl;
 }
