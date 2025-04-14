369c331e66c7c72937293103168cff8e1ebf8327
NDI05 <andhikap877@gmail.com>
2025-04-14 11:25:24 +0700

    feat: add new struct player in this file

---
+++ struct3.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Player {
+    string name;
+    string id;
+    string level;
+    Weapon wp;
+};
+
+struct Weapon {
+    string name;
+    string damage;
+};
+
+int main(){
+    Player pl;
+    cout << "==================================" << endl;
+    cout << "=====Input&Output Data Struct======" << endl;
+    cout << "==================================" << endl;
+    // input data to struct
+    cout << "Masukkan Nama Player : ";
+    getline(cin, pl.name);
+    cout << "Masukkan ID Player : ";
+    cin >> pl.id;
+    cout << "Masukkan Level Player : ";
+    cin >> pl.level;
+    // output data struct
+    cout << "=========Output Data Struct========" << endl;
+    cout << "Nama Player : " << pl.name << endl;
+    cout << "ID Player : " << pl.id << endl;
+    cout << "Level Player : " << pl.level << endl;
+    cout << "====================================" << endl;
+}
